9b93791755b13945311fad2f6b7b45f94854d66f
Sandesh Ghimire <sandesh@soccentric.com>
2026-08-05 15:13:11 -0700

    fix: add project definitions header

---
+++ include/showcase.h
@@ -0,0 +1,18 @@
+#ifndef SHOWCASE_H
+#define SHOWCASE_H
+
+/* Project definitions */
+#define NUM_PROJECTS 5
+#define PROJECT_CORTEX_FORGE 0
+#define PROJECT_IRIS_FLOW    1
+#define PROJECT_REEL_FORGE   2
+#define PROJECT_NEXUS_BUS    3
+#define PROJECT_VOLT_GUARD   4
+
+/* Tier definitions */
+#define NUM_TIERS 3
+#define TIER_DRIVER 0
+#define TIER_SERVER 1
+#define TIER_GUI    2
+
+#endif
